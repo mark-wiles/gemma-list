166878a074dac17fec14ed71032169846a3a2e3e
phpdev115 <goldendiscount01@gmail.com>
2018-12-13 02:11:51 -0800

    added post routes for glists and tasks

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/glists', 'GlistsController@store');
+
+Route::post('/glists/{glist}/task', 'TasksController@store');
+Route::patch('/tasks/{task}', 'TasksController@update');
