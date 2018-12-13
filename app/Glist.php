c0167650aa9b8c1edc7a489dd1c115097a6beec9
phpdev115 <goldendiscount01@gmail.com>
2018-12-13 02:08:52 -0800

    updated models

--- app/Glist.php
+++ app/Glist.php
@@ -8,8 +8,18 @@ class Glist extends Model
 {
 	protected $fillable = ['name', 'description', 'archived', 'user_id'];
 
-	  public function tasks()
+	public function user()
+	  {
+		return $this->belongsTo(User::class);
+	  }
+	
+	public function tasks()
 	  {
 		return $this->hasMany(Task::class);
-	  }
+		}
+		
+	public function addTask($task)
+		{
+		$this->tasks()->create($task);
+		}
 }
--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['title', 'description', 'completed', 'list_id'];
+
+    public function glist()
+	  {
+		return $this->belongsTo(Glist::class);
+		}
+		
+		public function complete($completed = true) {
+
+			$this->update(compact('completed'));
+
+	}
 }
