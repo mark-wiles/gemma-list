ed77c230f10c687bad1ef218a99a72cff6bd1263
phpdev115 <goldendiscount01@gmail.com>
2018-12-15 21:42:45 -0800

    updated glist validation

--- app/Http/Controllers/GlistsController.php
+++ app/Http/Controllers/GlistsController.php
@@ -52,7 +52,7 @@ class GlistsController extends Controller {
 
 		return request()->validate([
 
-            'name' => ['required', 'min:3']
+            'name' => ['required', 'min:3', 'max:20']
             
         ]);
 
