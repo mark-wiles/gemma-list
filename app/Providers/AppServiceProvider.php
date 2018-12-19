fca995d5a8f9f9eac5350e14de52124b40850a1f
phpdev115 <goldendiscount01@gmail.com>
2018-12-19 14:36:10 -0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap any application services.
      *
@@ -13,7 +16,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force SSL in production
+        if ($this->app->environment() == 'production') {
+            
+            URL::forceScheme('https');
+        }
+
     }
 
     /**
