08262aa6dc6ddfeb4909ee8b104864b9be5dd0c4
phpdev115 <goldendiscount01@gmail.com>
2019-12-31 17:15:53 -0800

    added order to glist model

--- app/Glist.php
+++ app/Glist.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Glist extends Model
 {
-	protected $fillable = ['name', 'description', 'archived', 'user_id'];
+	protected $fillable = ['name', 'description', 'archived', 'user_id', 'order'];
 
 	public function user()
 	  {
