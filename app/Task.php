f0d8714bb00322b6d36257a66f32fb05085f76f4
phpdev115 <goldendiscount01@gmail.com>
2020-01-18 14:55:04 -0800

    fixed spelling mistake

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'description', 'completed', 'list_id', 'order'];
+    protected $fillable = ['title', 'description', 'completed', 'glist_id', 'order'];
 
     public function glist()
 	  {
