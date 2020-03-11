4f83a5ffd475ea8e173fcd8eabd5513ec8a83d25
phpdev115 <goldendiscount01@gmail.com>
2020-03-11 00:31:15 -0700

    removed unnecessary comments

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 })->middleware('guest');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
 
 Route::post('/glists', 'GlistsController@store');
 Route::post('/glists/reorder', 'GlistsController@order');
