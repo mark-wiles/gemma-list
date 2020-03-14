b586bf6cd8a46f328a3ba29675c99af115c52245
phpdev115 <goldendiscount01@gmail.com>
2020-03-14 14:03:03 -0700

    added sourceMaps method

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').sourceMaps()
    .copy('resources/js/touch-punch.js', 'public/js/touch-punch.js')
    .sass('resources/sass/app.scss', 'public/css');
