d5bb316990e94072a96ca1683babec4ac8409ca8
phpdev115 <goldendiscount01@gmail.com>
2020-03-15 20:01:56 -0700

    re-added view name

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/logout', 'LoginController@logout');
 
