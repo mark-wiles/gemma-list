b2e63c087dd9ad36e136780a0cd1613be90d2ef4
phpdev115 <goldendiscount01@gmail.com>
2020-03-15 22:04:23 -0700

    updated mail_from address

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'support@gemmalist.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'gemmalist.app@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'GemmaList'),
     ],
 
