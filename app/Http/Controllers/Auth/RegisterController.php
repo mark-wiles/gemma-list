77fb2981d4be4d47f05c7eb96e308f7df1b30753
phpdev115 <goldendiscount01@gmail.com>
2021-02-11 02:41:33 -0800

    added custom validation message

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,11 +50,19 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
-        ]);
+        $messages = [
+            'name.regex' => 'Invalid Character used (<, >, &).',
+        ];
+
+        return Validator::make(
+            $data,
+            [
+                'name' => ['required', 'string', 'max:255', 'regex:/^[^<>&]+$/'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:6', 'confirmed'],
+            ],
+            $messages,
+        );
     }
 
     /**
