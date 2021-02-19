d730421916bd9526ec7f300f57a037af89384624
phpdev115 <goldendiscount01@gmail.com>
2021-02-19 01:27:51 -0800

    added share and confirm methods

--- app/Http/Controllers/GlistsController.php
+++ app/Http/Controllers/GlistsController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\User;
 use App\Glist;
+use App\SharedGlist;
+use App\Mail\SharedGlistMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class GlistsController extends Controller {
 	
@@ -76,7 +79,71 @@ class GlistsController extends Controller {
             'success' => true,
             'status' => 200
         ]);
-    }
+	}
+	
+	public function share(Glist $glist, Request $request) {
+		// Check if user exists
+		$sharedWithUser = User::select('id')->where(['email' => $request->email])->get();
+
+		// If user exists
+		if (count($sharedWithUser) > 0) {
+			$confirmCode = time();
+
+			// Set up attributes for new sharedGlist
+			$attributes['glist_id'] = $glist->id;
+			$attributes['user_id'] = $sharedWithUser[0]->id;
+			$attributes['confirm_code'] = $confirmCode;
+
+			// create sharedGlist
+			$shared = SharedGlist::create($attributes);
+
+			// Set up data to pass into email view
+			$shareData['email'] = $request->email;
+			$shareData['title'] = $glist->name;
+			$shareData['confirm'] = $confirmCode;
+			$shareData = (object) $shareData;
+
+			// Send email to sharee
+			Mail::to($request->email)->send(new SharedGlistMail($shareData));
+
+			// Return response status 200
+			return response()->json([
+				'success' => true,
+				'status' => 201,
+			]);
+
+		} else { // No user registered with that email address
+
+			return response()->json([
+				'success' => false,
+				'status' => 404,
+				'message' => 'No user account found for that email address',
+			]);
+
+		}
+
+	}
+
+	public function confirm(Request $request) {
+
+		// $confirm_code = $request->confirm;
+
+		$sharedGlist = SharedGlist::where([
+			'user_id' => auth()->id(),
+			'confirm_code' => $request->confirm
+		])->get();
+
+		if (count($sharedGlist) > 0) {
+			$sharedGlist[0]->update(['confirmed' => time()]);
+			return $sharedGlist;
+		} else {
+			return response()->json([
+				'success' => false,
+				'message' => 'Invalid request'
+			]);
+		}
+
+	}
     
     public function validateGlist() {
 
