ca3a4ff96cc485f002dce7e793dae1c7f2c91b2c
phpdev115 <goldendiscount01@gmail.com>
2022-01-12 00:03:52 -0800

    updated session length

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 10080),
 
     'expire_on_close' => false,
 
@@ -194,6 +194,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => 'strict',
 
 ];
